211fe89463bf13533952f07ad363bf23bc9cf7ad
willianleman <willian@spocket.co>
2024-08-31 23:11:59 -0300

    feat: remove dashboard for awhile

--- apps/web/app/(protected)/dashboard/layout.tsx
+++ apps/web/app/(protected)/dashboard/layout.tsx
@@ -1,13 +1,16 @@
 import { PropsWithChildren } from "react";
 import { Sidebar } from "./sidebar";
+import { redirect } from "next/navigation";
 
 export default async function DashboardLayout({ children }: PropsWithChildren) {
-  return (
-    <div className="min-h-screen bg-gray-100 p-8">
-      <div className="grid grid-cols-[20%_auto] gap-0 container">
-        <Sidebar />
-        {children}
-      </div>
-    </div>
-  );
+  return redirect("/");
+
+  // return (
+  //   <div className="min-h-screen bg-gray-100 p-8">
+  //     <div className="grid grid-cols-[20%_auto] gap-0 container">
+  //       <Sidebar />
+  //       {children}
+  //     </div>
+  //   </div>
+  // );
 }
