65568ad806d0fa58b30a8c512b420186c568f9a5
willianleman <willian@spocket.co>
2024-09-02 12:17:59 -0300

    feat: get user validate

--- apps/web/data-access/get-user.ts
+++ apps/web/data-access/get-user.ts
@@ -1,13 +1,15 @@
 import { supabaseDB } from "@/lib/supabase";
 
 export async function getUser({ id: userId }: { id: string }) {
+  if (!userId) return null;
+
   const { data: user } = await supabaseDB
     .from("users")
     .select("*, agents(*)")
     .eq("userAuthId", userId)
     .single()
     .then((res) => {
-      console.log(res)
+      console.log(res);
       const { agents, ...restUser } = res.data;
       const [agent] = agents;
       return {
